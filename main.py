4a9c959b7c2ff4258ee4e67ce7b0bd10dd2a6469
farhanrk <farhanrkbb@gmail.com>
2023-06-17 11:36:54 -0230

    going at it

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello world!")
+import random
+
+input = input("\n\n\nWanna play some Rock Paper Scissors with a bot and see how you fare? (y/n)\n").lower()
+
+if (input == y):
+    runGame = True
+    while runGame==True:
+        input = input("ROCK, PAPER or SCISSORS!? ").lower()
+        if (input == "rock" or "r" or "rocks"):
+            ##player rocks
+            print("player rocks")
+        elif (input == "paper" or "p" or "papers"):
+            ##player papers
+            print("player tries to fling around a paper")
+        elif (input == "scissors" or "s" or "scissor"):
+            ##player shreds
+            print("player SHREDS~~!!")
+        else:
+            ##invalid input
+            print("bruh")
