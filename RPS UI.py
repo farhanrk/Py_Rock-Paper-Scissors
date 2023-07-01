fd824e8c23aae1054135fc5a043242c8856fd011
farhanrk <farhanrkbb@gmail.com>
2023-07-01 09:53:50 -0230

    trying GUI

--- RPS UI.py
+++ RPS UI.py
@@ -29,6 +29,15 @@ label_a = tk.Label(master=frame_a, text="I'm in Frame A")
 label_a.pack()
 
 label_b = tk.Label(master=frame_b, text="I'm in Frame B")
+
+frame1 = tk.Frame(master=window, height=100, width=50, bg="red")
+frame1.pack(fill=tk.BOTH)
+
+frame2 = tk.Frame(master=window, height=50, width=50,bg="yellow")
+frame2.pack(fill=tk.BOTH)
+
+frame3 = tk.Frame(master=window, height=25, width=50, bg="blue")
+frame3.pack(fill=tk.BOTH)
 label_b.pack()
 
 frame_a.pack()
